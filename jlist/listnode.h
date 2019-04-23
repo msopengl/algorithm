198c3a2129cc0783c9feb9268d0755b1ad78e149
msopengl <msopengl@163.com>
2019-04-23 08:39:14 +0800

    add

--- jlist/listnode.h
+++ jlist/listnode.h
@@ -1,4 +1,5 @@
 #pragma once
+// other: https://blog.csdn.net/qq_30611601/article/details/79516986#%E5%9B%9B%E7%BA%BF%E6%80%A7%E8%A1%A8%E7%9A%84%E9%93%BE%E5%BC%8F%E5%AD%98%E5%82%A8
 
 class CListNode
 {
